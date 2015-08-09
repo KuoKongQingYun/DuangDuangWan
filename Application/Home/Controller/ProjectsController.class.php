c4f8265dd73425b7f378099d727033057bc084c6
KuoKongQingYun <609898048@qq.com>
2015-08-10 00:41:37 +0800

    备份进度
    备份进度

--- Application/Home/Controller/ProjectsController.class.php
+++ Application/Home/Controller/ProjectsController.class.php
@@ -3,21 +3,38 @@ namespace Home\Controller;
 use Think\Controller;
 class ProjectsController extends Controller {
     public function index(){
+        $videoModel=M('Video');
         $userModel=M('User');
-    	$userLogin['id']=I('cookie.UserID');
-    	$userLogin['password']=I('cookie.Key');
-    	$userData=$userModel->where($userLogin)->find();
-    	if(is_null($userData))
-    	{
-            $this->redirect('Home/Login/index');
-    	}
-    	else
-    	{
-    		$this->assign('button1title',$userData['name']);
+        $videoData=$videoModel->where(array('pid' =>0,'type'=>0))->select();
+        foreach ($videoData as &$value) {
+            $value['url']=U('Home/Video/index',array('id'=>$value['id']));
+            $ch=curl_init();
+            curl_setopt($ch, CURLOPT_URL, "https://openapi.youku.com/v2/videos/show.json?client_id=78cb1297cad3da80&video_id=".$value['vid']);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+            $data=curl_exec($ch);
+            $jsonData=json_decode($data);
+            $value['img']=$jsonData->{'bigThumbnail'};
+        }
+        $this->assign('videoList',$videoData);
+        $userLogin['id']=I('cookie.UserID');
+        $userLogin['password']=I('cookie.Key');
+        $userData=$userModel->where($userLogin)->find();
+        if(is_null($userData))
+        {
+            $this->assign('button1title','登陆');
+            $this->assign('button1url',U("Home/Login/index"));
+            $this->assign('button2title','注册');
+            $this->assign('button2url',U("Home/Register/index"));
+            $this->display();
+        }
+        else
+        {
+            $this->assign('button1title',$userData['name']);
             $this->assign('button1url',U("Home/User/index"));
             $this->assign('button2title','退出登陆');
             $this->assign('button2url',U("Home/Login/logout"));
-    		$this->display();
-    	}
+            $this->display();
+        }
     }
 }
--- Application/Home/Controller/VideoController.class.php
+++ Application/Home/Controller/VideoController.class.php
@@ -1,27 +1,38 @@
 <?php
 namespace Home\Controller;
 use Think\Controller;
-class IndexController extends Controller {
+class VideoController extends Controller {
     public function index(){
-    	$userModel=M('User');
-    	$userLogin['id']=I('cookie.UserID');
-    	$userLogin['password']=I('cookie.Key');
-    	$userData=$userModel->where($userLogin)->find();
-    	if(is_null($userData))
+        $videoModel=M('Video');
+        $userModel=M('User');
+
+    	$videoData=$videoModel->where(array(id=>I('get.id')))->find();
+    	if(is_null($videoData))
     	{
-            $this->assign('button1title','登陆');
-            $this->assign('button1url',U("Home/Login/index"));
-            $this->assign('button2title','注册');
-            $this->assign('button2url',U("Home/Register/index"));
-    		$this->display();
+            $this->redirect('Home/Projects/index');
     	}
     	else
     	{
-    		$this->assign('button1title',$userData['name']);
-            $this->assign('button1url',U("Home/User/index"));
-            $this->assign('button2title','退出登陆');
-            $this->assign('button2url',U("Home/Login/logout"));
-    		$this->display();
+            $this->assign('vid',$videoData['vid']);
+            $userLogin['id']=I('cookie.UserID');
+            $userLogin['password']=I('cookie.Key');
+            $userData=$userModel->where($userLogin)->find();
+            if(is_null($userData))
+            {
+                $this->assign('button1title','登陆');
+                $this->assign('button1url',U("Home/Login/index"));
+                $this->assign('button2title','注册');
+                $this->assign('button2url',U("Home/Register/index"));
+                $this->display();
+            }
+            else
+            {
+                $this->assign('button1title',$userData['name']);
+                $this->assign('button1url',U("Home/User/index"));
+                $this->assign('button2title','退出登陆');
+                $this->assign('button2url',U("Home/Login/logout"));
+                $this->display();
+            }
     	}
     }
 }
